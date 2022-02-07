cc33a67f336bc6cc2b1249ee57a50ce3a9de85b9
Zachary Hulcher <zhulcher@gmail.com>
2022-02-06 23:08:47 -0800

    Update Supera.cxx

--- Supera.cxx
+++ Supera.cxx
@@ -1,7 +1,10 @@
 #include "Supera.h"
 
+#if __has_include("larcv3/core/dataformat/Particle.h")
 void init_Supera(pybind11::module m)
 {
-    //init_SuperaLorentz(m);
+    // init_SuperaLorentz(m);
     init_superabase(m);
 }
+#endif
+
