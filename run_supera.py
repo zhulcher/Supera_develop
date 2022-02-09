d0e006b17c06b39d1bd91b1408f53e64af978cc6
Zachary Hulcher <zhulcher@gmail.com>
2022-02-08 20:05:34 -0800

    Update run_supera.py

--- run_supera.py
+++ run_supera.py
@@ -42,57 +42,85 @@ event_range = (0, ch.GetEntries())
 print('Processing', event_range[1] - event_range[0], 'events')
 sys.stdout.flush()
 
+# # Initialize and retrieve a list of processes that belongs to SuperaBase inherited module classes
+# proc.initialize()
+# supera_procs = []
+# ptrs = []
+# #module = proc.process_ptr(0)
+# #module = proc.process_ptr(1)
+# #module = proc.process_ptr(0)
+# #print("made it ")
+# #print(proc.process_names())
+# for name in proc.process_names():
+# 	pid = proc.process_id(name)
+# 	ptrs.append(proc.process_ptr(pid))
+# 	#print(pid, type(pid))
+# 	#print("hi kazu22")
+# 	#module=
+# 	#print("hi222")
+# 	#mod2=proc.process_ptr(0)
+# 	#print("hi322")
+# 	#mod3=proc.process_ptr(0)
+# 	#print("hi422")
+# 	#print("name:",name)
+# 	#print(dir(module))
+
+# 	if getattr(ptrs[-1], 'is')('Supera'):
+# 		print('Running a Supera module:', name)
+# 		supera_procs.append(pid)
+
+# print("All the Supera Procs ", supera_procs)
+# # Event loop
+
+
+# for entry in xrange(*event_range):
+# 	print("considering event:", entry)
+# 	sys.stdout.flush()
+# 	bytes = ch.GetEntry(entry)
+# 	if bytes < 1:
+# 		break
+# 	ev = ch.Event
+
+# 	proc.set_id(ev.RunId, 0, ev.EventId)
+# 	# set event pointers
+# 	for pid in supera_procs:
+# 		#module2 = proc.process_ptr(pid) #pybind scope issue without 2
+# 		print("pid", pid)
+# 		print(type(ev))
+# 		print(ev.__dict__.keys())
+# 		ptrs[pid].SetEvent(ev)
+# 		print("got here8")
+
+# 	proc.process_entry()
+# proc.finalize()
+
+
 # Initialize and retrieve a list of processes that belongs to SuperaBase inherited module classes
 proc.initialize()
 supera_procs = []
-
-ptrs = []
-
-
-#module = proc.process_ptr(0)
-#module = proc.process_ptr(1)
-#module = proc.process_ptr(0)
-#print("made it ")
-#print(proc.process_names())
 for name in proc.process_names():
 	pid = proc.process_id(name)
-	ptrs.append(proc.process_ptr(pid))
-	#print(pid, type(pid))
-	#print("hi kazu22")
-	#module=
-	#print("hi222")
-	#mod2=proc.process_ptr(0)
-	#print("hi322")
-	#mod3=proc.process_ptr(0)
-	#print("hi422")
-	#print("name:",name)
-	#print(dir(module))
-
-	if getattr(ptrs[-1], 'is')('Supera'):
-		print('Running a Supera module:', name)
+	module = proc.process_ptr(pid)
+	if getattr(module,'is')('Supera'):
+		print('Running a Supera module:',name)
 		supera_procs.append(pid)
 
-print("All the Supera Procs ", supera_procs)
 # Event loop
-
-
 for entry in xrange(*event_range):
 	print("considering event:", entry)
 	sys.stdout.flush()
 	bytes = ch.GetEntry(entry)
 	if bytes < 1:
 		break
-	ev = ch.Event
 
-	proc.set_id(ev.RunId, 0, ev.EventId)
+	ev = ch.Event 
+
+	proc.set_id(ev.RunId,0,ev.EventId)
+
 	# set event pointers
 	for pid in supera_procs:
-		#module2 = proc.process_ptr(pid) #pybind scope issue without 2
-		print("pid", pid)
-		print(type(ev))
-		print(ev.__dict__.keys())
-		ptrs[pid].SetEvent(ev)
-		print("got here8")
+		module = proc.process_ptr(pid)
+		module.SetEvent(ev)
 
 	proc.process_entry()
 proc.finalize()
