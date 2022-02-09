e0db857ad48cfc7f18912573a6447ce072dc0ef4
Zachary Hulcher <zhulcher@gmail.com>
2022-02-09 02:54:52 -0800

    Update run_supera.py

--- run_supera.py
+++ run_supera.py
@@ -28,91 +28,97 @@ if len(sys.argv) > 2:
 		ch.AddFile(argv)
 print("Chain has", ch.GetEntries(), "entries")
 # sometimes num_entry overflows instead of being negative.  not sure why?
-event_range = (proc.batch_start_entry(), proc.batch_start_entry() + proc.batch_num_entry()) \
-	if proc.batch_num_entry() > 0 and proc.batch_num_entry() < 1e18 \
-	else (0, min(ch.GetEntries(),50))
+#event_range = (proc.batch_start_entry(), proc.batch_start_entry() + proc.batch_num_entry()) \
+#	if proc.batch_num_entry() > 0 and proc.batch_num_entry() < 1e18 \
+#	else (0, min(ch.GetEntries(),50))
+event_range=(0, min(ch.GetEntries(),50))
 print('Processing', event_range[1] - event_range[0], 'events')
 sys.stdout.flush()
-
-# # Initialize and retrieve a list of processes that belongs to SuperaBase inherited module classes
-# proc.initialize()
-# supera_procs = []
-# ptrs = []
-# #module = proc.process_ptr(0)
-# #module = proc.process_ptr(1)
-# #module = proc.process_ptr(0)
-# #print("made it ")
-# #print(proc.process_names())
-# for name in proc.process_names():
-# 	pid = proc.process_id(name)
-# 	ptrs.append(proc.process_ptr(pid))
-# 	#print(pid, type(pid))
-# 	#print("hi kazu22")
-# 	#module=
-# 	#print("hi222")
-# 	#mod2=proc.process_ptr(0)
-# 	#print("hi322")
-# 	#mod3=proc.process_ptr(0)
-# 	#print("hi422")
-# 	#print("name:",name)
-# 	#print(dir(module))
-
-# 	if getattr(ptrs[-1], 'is')('Supera'):
-# 		print('Running a Supera module:', name)
-# 		supera_procs.append(pid)
-
-# print("All the Supera Procs ", supera_procs)
-# # Event loop
-
-
-# for entry in xrange(*event_range):
-# 	print("considering event:", entry)
-# 	sys.stdout.flush()
-# 	bytes = ch.GetEntry(entry)
-# 	if bytes < 1:
-# 		break
-# 	ev = ch.Event
-
-# 	proc.set_id(ev.RunId, 0, ev.EventId)
-# 	# set event pointers
-# 	for pid in supera_procs:
-# 		#module2 = proc.process_ptr(pid) #pybind scope issue without 2
-# 		print("pid", pid)
-# 		print(type(ev))
-# 		print(ev.__dict__.keys())
-# 		ptrs[pid].SetEvent(ev)
-# 		print("got here8")
-
-# 	proc.process_entry()
-# proc.finalize()
-
 
 # Initialize and retrieve a list of processes that belongs to SuperaBase inherited module classes
 proc.initialize()
 supera_procs = []
+ptrs = []
+#modulelist=[[None]*len(proc.process_names())]*min(ch.GetEntries(),50)
+#module = proc.process_ptr(0)
+#module = proc.process_ptr(1)
+#module = proc.process_ptr(0)
+#print("made it ")
+#print(proc.process_names())
 for name in proc.process_names():
 	pid = proc.process_id(name)
-	module = proc.process_ptr(pid)
-	if getattr(module,'is')('Supera'):
-		print('Running a Supera module:',name)
-		supera_procs.append(pid)
+	ptrs.append(proc.process_ptr(pid))
+	modulelist=[[]]
+	#print(pid, type(pid))
+	#print("hi kazu22")
+	#module=
+	#print("hi222")
+	#mod2=proc.process_ptr(0)
+	#print("hi322")
+	#mod3=proc.process_ptr(0)
+	#print("hi422")
+	#print("name:",name)
+	#print(dir(module))
 
+	if getattr(ptrs[-1], 'is')('Supera'):
+		print('Running a Supera module:', name)
+		supera_procs.append(pid)
+		
+
+print("All the Supera Procs ", supera_procs)
 # Event loop
+
+
 for entry in xrange(*event_range):
 	print("considering event:", entry)
 	sys.stdout.flush()
 	bytes = ch.GetEntry(entry)
 	if bytes < 1:
 		break
+	ev = ch.Event
 
-	ev = ch.Event 
-
-	proc.set_id(ev.RunId,0,ev.EventId)
-
+	proc.set_id(ev.RunId, 0, ev.EventId)
 	# set event pointers
 	for pid in supera_procs:
-		module = proc.process_ptr(pid)
-		module.SetEvent(ev)
+		#print(pid,ev.EventId)
+		#modulelist[pid][ev.EventId] = proc.process_ptr(pid) #pybind scope issue without 2
+		#print("pid", pid)
+		#print(type(ev))
+		#print(ev.__dict__.keys())
+		#modulelist[pid][ev.EventId].SetEvent(ev)
+		(proc.process_ptr(pid)).SetEvent(ev)
+		#print("got here8")
 
 	proc.process_entry()
 proc.finalize()
+
+
+# # Initialize and retrieve a list of processes that belongs to SuperaBase inherited module classes
+# proc.initialize()
+# supera_procs = []
+# for name in proc.process_names():
+# 	pid = proc.process_id(name)
+# 	module = proc.process_ptr(pid)
+# 	if getattr(module,'is')('Supera'):
+# 		print('Running a Supera module:',name)
+# 		supera_procs.append(pid)
+
+# # Event loop
+# for entry in xrange(*event_range):
+# 	print("considering event:", entry)
+# 	sys.stdout.flush()
+# 	bytes = ch.GetEntry(entry)
+# 	if bytes < 1:
+# 		break
+
+# 	ev = ch.Event 
+
+# 	proc.set_id(ev.RunId,0,ev.EventId)
+
+# 	# set event pointers
+# 	for pid in supera_procs:
+# 		module = proc.process_ptr(pid)
+# 		module.SetEvent(ev)
+
+# 	proc.process_entry()
+# proc.finalize()
